286f8085018f839af9c816791816232e0153545f
yigityanik <yeyanik123@gmail.com>
2021-07-10 13:31:55 +0300

    GM8_sprout: update DPM blobs from daisy-user-10-QKQ1.191002.002-V11.0.18.0.QDLMIXM-release-keys

--- Android.mk
+++ Android.mk
@@ -235,7 +235,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := dpmserviceapp
 LOCAL_MODULE_OWNER := GM
-LOCAL_SRC_FILES := proprietary/priv-app/dpmserviceapp/dpmserviceapp.apk
+LOCAL_SRC_FILES := proprietary/product/priv-app/dpmserviceapp/dpmserviceapp.apk
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
